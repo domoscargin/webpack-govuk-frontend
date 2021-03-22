e57ce5c48ad8238999b5ef0dda6493012f8e8626
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2021-03-22 19:03:25 +0000

    Common JS format

---
+++ src/js/app-commonjs-format.js
@@ -0,0 +1,15 @@
+// CommonJS module format
+const GOVUKFrontend = require('govuk-frontend')
+const Accordion = GOVUKFrontend.Accordion
+const CharacterCount = GOVUKFrontend.CharacterCount
+
+var $accordion = document.querySelector('[data-module="govuk-accordion"]')
+if ($accordion) {
+  new Accordion($accordion).init()
+}
+var $characterCount = document.querySelector('[data-module="govuk-character-count"]')
+if ($characterCount) {
+  new CharacterCount($characterCount).init()
+}
+
+require('./main.js')
