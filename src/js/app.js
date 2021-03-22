fab369389b496bc69ff57039f596e671986b9d0e
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2021-03-22 17:41:03 +0000

    Import two components with CommonJS syntax

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
+// CommonJS require syntax
 const GOVUKFrontend = require('govuk-frontend')
-GOVUKFrontend.initAll()
+const Accordion = GOVUKFrontend.Accordion
+const CharacterCount = GOVUKFrontend.CharacterCount
+
+var $accordion = document.querySelector('[data-module="govuk-accordion"]')
+if ($accordion) {
+  new Accordion($accordion).init()
+}
+var $characterCount = document.querySelector('[data-module="govuk-character-count"]')
+if ($characterCount) {
+  new CharacterCount($characterCount).init()
+}
 
 require('./main.js')
